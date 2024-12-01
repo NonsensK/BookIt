22bb859487627e865f8be56d1e7d0198f0c4c2d1
Daniil Krylov <daniilkrylov@Air-Daniil.lan>
2024-12-02 00:10:50 +0100

    Initial commit

---
+++ src/main/java/com/example/migration/TestServlet.java
@@ -0,0 +1,16 @@
+package com.example.migration;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello, this is TestServlet running with Jakarta Servlet API!");
+    }
+}
